21756d1e4993b56faa7891f0d122fda8ca06d667
kzime <kzime@qq.com>
2021-11-24 22:38:57 +0800

    p1 finished

--- Assets/Scripts/BeatScrooler.cs
+++ Assets/Scripts/BeatScrooler.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BeatScrooler : MonoBehaviour
 {
+    public float bpm;
+    public bool hasStarted;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bpm = bpm / 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted) {
+            if (Input.anyKeyDown) {
+                hasStarted = true;
+            }         
+        } else {
+            transform.position -= new Vector3(0f, bpm * Time.deltaTime, 0f);
+        }
+
     }
 }
---
+++ Assets/Scripts/NotesObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesObject : MonoBehaviour
+{
+
+    public bool canBepress;
+    public KeyCode keyToPress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress)) {
+            if (canBepress) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Activator")
+            canBepress = true;
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Activator")
+            canBepress = false;
+    }
+}
